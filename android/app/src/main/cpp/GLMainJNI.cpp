9e5c9e0f8020261a65a6098f38f864e32f6e68e2
forzaster <forzaster@gmail.com>
2017-01-01 02:02:00 +0900

    Changed folder structure

--- android/app/src/main/cpp/GLMainJNI.cpp
+++ android/app/src/main/cpp/GLMainJNI.cpp
@@ -4,7 +4,7 @@
 
 #include <jni.h>
 #include <string>
-#include "common/GLMain.h"
+#include "common/graphics/GLMain.h"
 
 extern "C" {
 JNIEXPORT void JNICALL Java_com_forzaster_cameramemoz_GLMain_init(JNIEnv * env, jobject obj,  jint width, jint height);
